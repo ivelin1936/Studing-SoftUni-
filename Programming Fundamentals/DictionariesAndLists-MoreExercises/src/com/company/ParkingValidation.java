51cac02264da146238ae27c56ce69ff65130baa6
Ivelin1936 <ivelin_1936@abv.bg>
2017-10-24 20:54:42 +0300

    update Dictionaries and Lists - More Exercises

---
+++ Programming Fundamentals/DictionariesAndLists-MoreExercises/src/com/company/ParkingValidation.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ParkingValidation {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
