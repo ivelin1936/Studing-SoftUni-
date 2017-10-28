4870b2f8097a3bd1889404d714145541992f4406
Ivelin1936 <ivelin_1936@abv.bg>
2017-10-28 22:50:41 +0300

    update Regular Expressions RegEx - Exercises

--- Programming Fundamentals/RegularExpressionsRegEx-Exercises/src/com/company/ValidUsernames.java
+++ Programming Fundamentals/RegularExpressionsRegEx-Exercises/src/com/company/ValidUsernames.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ValidUsernames {
     public static void main(String[] args) throws IOException {
@@ -12,6 +15,31 @@ public class ValidUsernames {
 
         ArrayList<String> myList = new ArrayList<>(Arrays.asList(reader.readLine().split("[\\s/\\\\()]")));
 
-        System.out.println(myList);
+        List<String> sortedList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("(\\b[A-Za-z][\\w+]{2,24}\\b)");
+
+        for (String s : myList) {
+            Matcher matcher = pattern.matcher(s);
+
+            if (matcher.find()) {
+                sortedList.add(s);
+            }
+        }
+
+        int lengthCounter = 0;
+        for (int i = 1; i < sortedList.size(); i++) {
+            int momentLength = sortedList.get(i).length() + sortedList.get(i - 1).length();
+
+            if (momentLength > lengthCounter) {
+                lengthCounter = momentLength;
+                myList.clear();
+                myList.add(sortedList.get(i - 1));
+                myList.add(sortedList.get(i));
+            }
+        }
+
+        for (String userName : myList) {
+            System.out.println(userName);
+        }
     }
 }
