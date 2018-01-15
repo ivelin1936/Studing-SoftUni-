9c4c679cf8651b7c62603fea5a16b3ccad4f6987
Ivelin1936 <ivelin_1936@abv.bg>
2018-01-15 18:23:11 +0200

    update Java OOP Basics Exercises Methods

---
+++ Java OOP Basics/Java OOP Basics Exercises Methods/src/com/company/DrawingTool/Square.java
@@ -0,0 +1,22 @@
+package com.company.DrawingTool;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class Square implements Figure{
+
+    private int size;
+
+    public Square(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void draw() {
+        System.out.printf("|%s|%n", new String(new char[this.size]).replace("\0", "-"));
+        for (int i = 0; i < this.size - 2; i++) {
+            System.out.printf("|%s|%n", new String(new char[this.size]).replace("\0", " "));
+        }
+
+        System.out.printf("|%s|%n", new String(new char[this.size]).replace("\0", "-"));
+    }
+}
