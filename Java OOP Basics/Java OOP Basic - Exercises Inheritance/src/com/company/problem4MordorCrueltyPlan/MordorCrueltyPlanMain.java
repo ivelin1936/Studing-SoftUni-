975e6c96b03f06070c46884b0451d57b1a69dac3
Ivelin1936 <ivelin_1936@abv.bg>
2018-01-20 19:01:19 +0200

    update Java OOP Basic - Exercises Inheritance

---
+++ Java OOP Basics/Java OOP Basic - Exercises Inheritance/src/com/company/problem4MordorCrueltyPlan/MordorCrueltyPlanMain.java
@@ -0,0 +1,12 @@
+package com.company.problem4MordorCrueltyPlan;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MordorCrueltyPlanMain {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
