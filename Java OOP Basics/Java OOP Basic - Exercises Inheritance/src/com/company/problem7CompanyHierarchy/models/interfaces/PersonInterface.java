9a1983b3f3c7f615908c51a93429efc476581503
Ivelin1936 <ivelin_1936@abv.bg>
2018-01-21 17:38:39 +0200

    update Java OOP Basic - Exercises Inheritance

--- Java OOP Basics/Java OOP Basic - Exercises Inheritance/src/com/company/problem7CompanyHierarchy/models/interfaces/PersonInterface.java
+++
@@ -1,8 +0,0 @@
-package com.company.problem7CompanyHierarchy.models.interfaces;
-
-public interface PersonInterface {
-    public int id = 0;
-    public String firstName = null;
-    public String lastName = null;
-
-}
