037cb34d4d6b0b655222e36feaa3293f94ed8aba
Ivelin1936 <ivelin_1936@abv.bg>
2018-02-14 18:02:02 +0200

    update Exercises - Subqueries and JOINs

--- Database Basics – MySQL/Exercises/Exercises - Subqueries and JOINs.sql
+++ Database Basics – MySQL/Exercises/Exercises - Subqueries and JOINs.sql
@@ -244,7 +244,34 @@ LIMIT 5;
 -- 15.*Continents and Currencies -------------------------------------
 -- -------------------------------------------------------------------
 
-
+SELECT 
+    c2.continent_code, c2.currency_code, c2.currency_usage
+FROM
+    (SELECT 
+        c.continent_code,
+            c.currency_code,
+            MAX(c.currency_usage) AS currency_usage
+    FROM
+        (SELECT 
+        continent_code,
+            currency_code,
+            COUNT(currency_code) AS currency_usage
+    FROM
+        countries
+    GROUP BY continent_code , currency_code) AS c
+    GROUP BY c.continent_code
+    HAVING currency_usage != 1) AS c1
+        JOIN
+    (SELECT 
+        continent_code,
+            currency_code,
+            COUNT(currency_code) AS currency_usage
+    FROM
+        countries
+    GROUP BY continent_code , currency_code) AS c2 ON c1.continent_code = c2.continent_code
+WHERE
+    c2.currency_usage = c1.currency_usage
+ORDER BY c2.continent_code , c2.currency_usage;
 
 -- 16.Countries without any Mountains --------------------------------
 -- -------------------------------------------------------------------
