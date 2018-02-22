42ebf884993ac90bca25b54b948758efdae643bd
Ivelin1936 <ivelin_1936@abv.bg>
2018-02-22 13:33:26 +0200

    update Database Basics (MySQL) Exam Preparation I – Airport Management System

--- Database Basics – MySQL/Exams/Database Basics (MySQL) Exam Preparation I – Airport Management System.sql
+++ Database Basics – MySQL/Exams/Database Basics (MySQL) Exam Preparation I – Airport Management System.sql
@@ -284,12 +284,46 @@ ORDER BY age DESC , c.customer_id ASC;
 -- 15.Airports and Passengers -------------------------------------------------
 -- ----------------------------------------------------------------------------
 
+SELECT 
+    a.airport_id,
+    a.airport_name,
+    COUNT(t.customer_id) AS 'passengers'
+FROM
+    `airports` AS a
+        JOIN
+    `flights` AS f ON f.origin_airport_id = a.airport_id
+        JOIN
+    `tickets` AS t ON t.flight_id = f.flight_id
+WHERE
+    f.status LIKE 'Departing'
+GROUP BY a.airport_id , a.airport_name
+ORDER BY a.airport_id ASC;
+
+-- Section 4: Programmability -------------------------------------------------
+
+CREATE TABLE customer_reviews(
+	review_id INT AUTO_INCREMENT,
+	review_content VARCHAR(255) NOT NULL,
+	review_grade INT NOT NULL,
+	airline_id INT,
+	customer_id INT,
+	CONSTRAINT pk_customer_reviews PRIMARY KEY(review_id),
+	CONSTRAINT fk_customer_reviews_airlines FOREIGN KEY(airline_id) REFERENCES airlines(airline_id),
+	CONSTRAINT fk_customer_reviews_customers FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE customer_bank_accounts(
+	account_id INT AUTO_INCREMENT,
+	account_number VARCHAR(10) UNIQUE NOT NULL,
+	balance DECIMAL(10, 2) NOT NULL,
+	customer_id INT,
+	CONSTRAINT pk_customer_bank_accounts PRIMARY KEY(account_id),
+	CONSTRAINT fk_customer_bank_accounts_customers FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
 
 
-
-
-
-
+-- 16.Submit Review -----------------------------------------------------------
+-- ----------------------------------------------------------------------------
 
 
 
