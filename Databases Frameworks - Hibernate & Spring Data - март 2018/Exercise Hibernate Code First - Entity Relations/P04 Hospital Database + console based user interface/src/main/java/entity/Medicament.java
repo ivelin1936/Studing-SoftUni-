129250979f3f077130644ab2767d99ac5d1b4497
Ivelin1936 <ivelin_1936@abv.bg>
2018-03-26 00:32:02 +0300

    add Exercise Hibernate Code First - Entity Relations

---
+++ Databases Frameworks - Hibernate & Spring Data - март 2018/Exercise Hibernate Code First - Entity Relations/P04 Hospital Database + console based user interface/src/main/java/entity/Medicament.java
@@ -0,0 +1,38 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "medicaments")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Medicament {
+
+    private Long id;
+    private String name;
+
+    public Medicament() {
+    }
+
+    public Medicament(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
