2e4786ae9e2eca95d24de3ff6e154b9a97a4f849
Ivelin1936 <ivelin_1936@abv.bg>
2018-06-15 22:58:50 +0300

    add Exercise - Encapsulation p05_pizzaCalories

--- Java Fundamentals/Java OOP Basic/Exercise - Encapsulation/src/p05_pizzaCalories/Main.java
+++ Java Fundamentals/Java OOP Basic/Exercise - Encapsulation/src/p05_pizzaCalories/Main.java
@@ -1,12 +1,49 @@
 package p05_pizzaCalories;
 
+import p05_pizzaCalories.models.Dough;
+import p05_pizzaCalories.models.Pizza;
+import p05_pizzaCalories.models.Topping;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static final String WHITE_SPACES_REGEX = "\\s+";
+    private static final String STOP_PROGRAM = "END";
+
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        try {
+            String[] pizzaTokens = reader.readLine().split(WHITE_SPACES_REGEX);
+            String pizzaName = pizzaTokens[1];
+            int numberOfToppings = Integer.parseInt(pizzaTokens[2]);
+            Pizza pizza = new Pizza(pizzaName, numberOfToppings);
+
+            String[] doughTokens = reader.readLine().split(WHITE_SPACES_REGEX);
+            String flourType = doughTokens[1];
+            String bakingTechnique = doughTokens[2];
+            int doughWeightInGrams = Integer.parseInt(doughTokens[3]);
+            Dough dough = new Dough(flourType, bakingTechnique, doughWeightInGrams);
+
+            pizza.setDough(dough);
+
+            String line;
+            while (!STOP_PROGRAM.equalsIgnoreCase(line = reader.readLine())) {
+                String[] toppingTokens = line.split(WHITE_SPACES_REGEX);
+                String toppingType = toppingTokens[1];
+                int toppingWeightInGrams = Integer.parseInt(toppingTokens[2]);
+
+                Topping topping = new Topping(toppingType, toppingWeightInGrams);
+                pizza.addTopping(topping);
+            }
+
+            System.out.println(pizza);
+
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+        }
     }
 }
