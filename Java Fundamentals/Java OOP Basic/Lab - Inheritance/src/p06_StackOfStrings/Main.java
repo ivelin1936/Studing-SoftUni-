1955164dac0c06cd2c1fd9a5986f1bd384cd1164
Ivelin1936 <ivelin_1936@abv.bg>
2018-06-18 13:41:34 +0300

    update Lab - Inheritance

--- Java Fundamentals/Java OOP Basic/Lab - Inheritance/src/p06_StackOfStrings/Main.java
+++ Java Fundamentals/Java OOP Basic/Lab - Inheritance/src/p06_StackOfStrings/Main.java
@@ -7,6 +7,17 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        StackOfStrings sos = new StackOfStrings();
 
+        sos.push("one");
+        sos.push("two");
+        sos.push("three");
+
+        System.out.println(sos.isEmpty());
+        System.out.println(sos.peek());
+
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
     }
 }
