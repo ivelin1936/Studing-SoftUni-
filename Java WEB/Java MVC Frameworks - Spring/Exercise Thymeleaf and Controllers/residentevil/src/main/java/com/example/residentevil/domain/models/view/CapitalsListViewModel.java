c634a2bc2a93a789c242301912c234f0829137fa
Ivelin1936 <ivelin_1936@abv.bg>
2019-03-15 19:13:43 +0200

    add Exercise Thymeleaf and Controllers/residentevil

---
+++ Java WEB/Java MVC Frameworks - Spring/Exercise Thymeleaf and Controllers/residentevil/src/main/java/com/example/residentevil/domain/models/view/CapitalsListViewModel.java
@@ -0,0 +1,26 @@
+package com.example.residentevil.domain.models.view;
+
+public class CapitalsListViewModel {
+
+    private String id;
+    private String name;
+
+    public CapitalsListViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
